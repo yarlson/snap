6cdce6f59605e4d2f71b92d00311a3c3a214d642
Yar Kravtsov <yarlson@gmail.com>
2026-02-27 11:17:49 +0200

    feat(ui): apply Info formatting to new command output

--- cmd/new.go
+++ cmd/new.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/yarlson/snap/internal/session"
+	"github.com/yarlson/snap/internal/ui"
 )
 
 var newCmd = &cobra.Command{
@@ -30,10 +31,10 @@ func newRun(cmd *cobra.Command, args []string) error {
 
 	fmt.Fprintln(cmd.OutOrStdout(), "Created session '"+name+"'")
 	fmt.Fprintln(cmd.OutOrStdout())
-	fmt.Fprintln(cmd.OutOrStdout(), "Next steps:")
-	fmt.Fprintln(cmd.OutOrStdout(), "  1. Plan your tasks: snap plan "+name)
-	fmt.Fprintln(cmd.OutOrStdout(), "  2. Or add task files manually to .snap/sessions/"+name+"/tasks/")
-	fmt.Fprintln(cmd.OutOrStdout(), "  3. Run tasks: snap run "+name)
+	fmt.Fprintln(cmd.OutOrStdout(), ui.Info("Next steps:"))
+	fmt.Fprint(cmd.OutOrStdout(), ui.Info("  1. Plan your tasks: snap plan "+name))
+	fmt.Fprint(cmd.OutOrStdout(), ui.Info("  2. Or add task files manually to .snap/sessions/"+name+"/tasks/"))
+	fmt.Fprint(cmd.OutOrStdout(), ui.Info("  3. Run tasks: snap run "+name))
 
 	return nil
 }
